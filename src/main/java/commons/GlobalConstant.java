935968e132ab6403ee51a9860269a7916969e670
hung <nguyenlephuhung15@gmail.com>
2025-03-10 20:53:49 +0700

    update BasePage with rest Parameter

---
+++ src/main/java/commons/GlobalConstant.java
@@ -0,0 +1,13 @@
+package commons;
+
+public class GlobalConstant {
+    public static final String PROJECT_PATH = System.getProperty("user.dir");
+    public static final String OS_NAME = System.getProperty("os.name");
+
+    public static final long SHORT_TIMEOUT =10;
+    public static final long LONG_TIMEOUT = 30;
+
+    public static final String DEV_ADMIN_URL="";
+
+    public static final String UPLOAD_PATH = PROJECT_PATH + "/uploadfile/";
+}
