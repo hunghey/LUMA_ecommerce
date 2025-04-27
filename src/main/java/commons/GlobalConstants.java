258edc890d3cafb15deec5f34cf5e96d3bff9d20
hung <nguyenlephuhung15@gmail.com>
2025-04-27 23:12:48 +0700

    add properties config

--- src/main/java/commons/GlobalConstants.java
+++ src/main/java/commons/GlobalConstants.java
@@ -2,10 +2,13 @@ package commons;
 
 import java.nio.file.Paths;
 
-public class GlobalConstant {
+public class GlobalConstants {
     public static final String PROJECT_PATH = System.getProperty("user.dir");
     public static final String OS_NAME = System.getProperty("os.name");
     public static final String SEPARATOR = System.getProperty("file.separator");
+
+    // data tt/ environment
+    public static final String ENVIRONMENT_CONFIG_PATH = PROJECT_PATH + SEPARATOR + "envConfig" + SEPARATOR;
 
     public static final long SHORT_TIMEOUT =10;
     public static final long LONG_TIMEOUT = 30;
